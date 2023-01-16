ff207ca58db0d0173813adcd36eaba713acacc24
Your Name <harshitgoyal213@gmail.com>
2023-01-17 00:02:15 +0530

    cart done i think

--- app/http/controllers/Customers/cartController.js
+++ app/http/controllers/Customers/cartController.js
@@ -41,16 +41,18 @@ function cartController(){
                     // console.log(user_cart);
                     let prevAvailable = false;
                     for(var i = 0;i<user_cart.items.length; i++){
-                        // console.log(cart_item.items)
-                        // console.log("lo",user_cart.items[i])
-                        // console.log(req.body._id);
-                        // console.log(user_cart.items[i].item)
                         if (user_cart.items[i].item == req.body._id){
                             // console.log("sldnvds");
+                            let old_item_list = user_cart.items;
+                            let item = user_cart.items;
+                            item[i].itemQty += 1;
+                            let update_cart = await Cart.findOneAndUpdate({ 'user_id': req.user._id },
+                                { items: item }
+                            );
+                            console.log(item[i].itemQty)
                             prevAvailable = true;                            
                         }
                     }
-                    console.log(prevAvailable)
                     if(!prevAvailable){
                         let old_item_list = user_cart.items;
                         let item = {item : req.body._id, itemQty : 1}
@@ -60,10 +62,7 @@ function cartController(){
                         //now our task is to update that part
                         let update_cart = await Cart.findOneAndUpdate({'user_id': req.user._id}, 
                             { items: old_item_list }
-                        );
-                        console.log(update_cart);
-                        // user_cart.items.push(itemX);
-                        
+                        );   
                     }
                 }
             }
@@ -72,37 +71,6 @@ function cartController(){
             }
 
         }
-
-
-        // updateCart(req, res){
-        // //for the first time when user have nothing in cart
-        //     if(!req.session.cart){
-        //         req.session.cart = {
-        //             items:{},
-        //             totalQty: 0,
-        //             totalPrice : 0
-        //         }
-        //     }
-        //     let cart = req.session.cart;
-        //     console.log(req.body);
-        //     // check if item is not exist in cart;
-        //     if(!cart.items[req.body._id]){
-        //         cart.items[req.body._id] = {
-        //             item : req.body,
-        //             qty : 1 
-        //         }
-        //     }
-        //     else{
-        //         cart.items[req.body._id]+=1;
-        //         cart.totalQty += 1;
-        //         cart.totalPrice += req.body.price;
-        //     }
-        //     return res.json({totalQty:req.session.cart.totalQty})
-        // }
-
-        // async updateCart(req, res){
-        //     let item = req.body;
-        // }
         
     }
 }
